e58dc5ca7bd62ab2360e531759f31ffc549a3f4e
Ed Carney <carneyedwardj@gmail.com>
2024-04-27 22:37:48 -0400

    New interface functions

--- db_handler.hpp
+++ db_handler.hpp
@@ -1,19 +1,30 @@
 #include <sqlite3.h>
+#include <string>
+#include <vector>
 
 namespace db_interface {
+
+    struct todo {
+        int id;
+        std::string text;
+    };
 
     class db_handler {
         public:
             db_handler(const char *db_file);
             ~db_handler();
 
-            int add_todo(const char *todo);
-            int update_todo(int id, const char *new_todo);
+            std::vector<todo> get_todos();
+            todo get_todo(int id);
+            int add_todo(std::string todo_text);
+            int update_todo(todo new_todo);
             int delete_todo(int id);
             int strikethrough_todo(int id);
 
         private:
             sqlite3 *db;
+            void handle_ret_code(int ret_code);
+            void initialize_db();
     };
 
 }

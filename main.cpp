1c877309ddfccb25b6abb3b39997e55fdf588203
Ed Carney <carneyedwardj@gmail.com>
2024-04-28 15:30:01 -0400

    Adds delete/add from command line

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <cstdlib>
 #include <sqlite3.h>
 #include "src/logger.hpp"
 #include "src/db_handler.hpp"
 
 using db_interface::todo;
 
-void print_all(db_interface::db_handler *handler) {
+void print_all_todos(db_interface::db_handler *handler) {
     std::vector<todo> todos = handler->get_todos();
     for (auto &t : todos) {
         std::cout << "id: " << t.id << ", todo: " << t.text << std::endl;
     }
-}
-
-void print_usage() {
-}
-
-void print_invalid_cmd(char *cmd) {
-    std::cout << "Invalid command: " << cmd << std::endl;
-    print_usage();
 }
 
 bool check_cmd_arg(char *cmd, std::vector<std::string> valid_cmds) {
@@ -32,9 +25,25 @@ bool check_cmd_arg(char *cmd, std::vector<std::string> valid_cmds) {
 }
 
 void add_todo(db_interface::db_handler *handler, char *cmd_arg) {
+    handler->add_todo((std::string)cmd_arg);
 }
 
 void delete_todo(db_interface::db_handler *handler, char *cmd_arg) {
+    int id = atoi(cmd_arg);
+    handler->delete_todo(id);
+}
+
+void print_usage() {
+    std::cout << "term-todos is a simple CLI todo tracker" << std::endl;
+    std::cout << "\tls/list: lists todos" << std::endl;
+    std::cout << "\ta[dd] TODO_TEXT: adds a new todo" << std::endl;
+    std::cout << "\td[elete] TODO_ID: deletes a todo by id" << std::endl;
+    std::cout << "\tu[pdate] TODO_ID TODO_TEXT: updates a todo with new text" << std::endl;
+}
+
+void print_invalid_cmd(char *cmd) {
+    std::cout << "Invalid command: " << cmd << std::endl;
+    print_usage();
 }
 
 int main(int argc, char* argv[]) {
@@ -52,13 +61,13 @@ int main(int argc, char* argv[]) {
 
     switch (argc) {
         case 1: {
-            // print usage
+            print_usage();
             break;
         }
         case 2: {
             char *cmd = argv[1];
             if (check_cmd_arg(cmd, { "ls", "list"})) {
-                print_all(handler);
+                print_all_todos(handler);
             } else {
                 print_invalid_cmd(cmd);
             }
